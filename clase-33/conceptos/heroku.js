e9be42f304d520a88dca991a4cb6a61f14f37293
Mariano Jimenez Perez <marianojimenezperez1@gmail.com>
2022-05-05 20:36:09 -0300

    clase 34

---
+++ clase-33/conceptos/heroku.js
@@ -0,0 +1,8 @@
+/*
+
+¿Qué es?
+Heroku es una plataforma en la nube que ofrece servicio para alojar e implementar aplicaciones web en varios lenguajes de programación, como Node.js, entre otros.
+Las aplicaciones se corren desde un servidor Heroku usando Heroku DNS Server para apuntar al dominio de la aplicación (nombreaplicacion.herokuapp.com). 
+Cada aplicación corre sobre un motor a través de una “red de bancos de prueba” que consta de varios servidores. El servidor Git de Heroku maneja los repositorios de las aplicaciones que son subidas por los usuarios.
+
+*/
---
+++ clase-33/conceptos/paas.js
@@ -0,0 +1,27 @@
+/*
+¿Qué es?
+
+Paas (plataforma como servicio) es un entorno de desarrollo e implementación completo en la nube.
+Cuenta con recursos que permiten generar “de todo”: desde aplicaciones sencillas basadas en la nube hasta aplicaciones empresariales sofisticadas habilitadas para la nube.
+Se compran los recursos que necesitamos a un proveedor de servicios en la nube, a los que accedemos a través de internet, pero solo pagamos por el uso que hacemos de ellos.
+PaaS incluye infraestructura (servidores, almacenamiento y redes), tanto como middleware, herramientas de desarrollo, servicios de inteligencia empresarial (BI), sistemas de administración de bases de datos, etc. 
+Está diseñado para sustentar el ciclo de vida completo de las aplicaciones web: compilación, pruebas, implementación, administración y actualización.
+Nos permite evitar el gasto y la complejidad que suponen la compra y la administración de licencias de software, la infraestructura de aplicaciones y el middleware subyacentes, los orquestadores de contenedores o las herramientas de desarrollo y otros recursos. 
+Administramos las aplicaciones y los servicios que desarrollamos y, normalmente, el proveedor de servicios en la nube administra todo lo demás.
+
+Escenarios habituales de uso de PaaS:
+
+    -Framework de desarrollo. PaaS proporciona un framework que los desarrolladores pueden ampliar para desarrollar o personalizar aplicaciones basadas en la nube. Permite a los desarrolladores crear aplicaciones usando componentes de software integrados.
+    -Análisis o inteligencia empresarial. Las herramientas que se proporcionan en PaaS permiten a las empresas realizar análisis de datos, obtener cierta información y entonces poder predecir los resultados para mejorar las previsiones de la empresa.
+    -Servicios adicionales. Los proveedores de PaaS pueden ofrecer otros servicios que mejoren las aplicaciones, como flujo de trabajo, 
+    directorios, seguridad y programación.
+
+Ventajas  del uso de PaaS:
+
+    -Reducir el tiempo de programación. Las herramientas de desarrollo de PaaS pueden reducir el tiempo que se tarda en programar aplicaciones nuevas con componentes de aplicación preprogramados.
+    -Agregar más funcionalidad de desarrollo sin incorporar más personal. Los componentes de plataforma como servicio pueden aportar al equipo de desarrollo nuevas características sin necesidad de contratar personal especializado.
+    -Desarrollar para varias plataformas con más facilidad. Algunos proveedores de servicios ofrecen opciones de desarrollo para varias plataformas, lo que agiliza y facilita el desarrollo de aplicaciones multiplataforma.
+    -Usar herramientas sofisticadas a un precio asequible. Gracias a un modelo de pago por uso, personas y empresas podemos usar software de desarrollo sofisticado y herramientas de inteligencia empresarial y análisis cuya compra no se podrían permitir.
+    -Colaboración en equipos de desarrollo distribuidos geográficamente. Como se accede a través de Internet, los equipos de desarrollo pueden colaborar en proyectos estando en países diferentes.
+    -Administrar el ciclo de vida de las aplicaciones con eficacia. PaaS proporciona todas las características necesarias para sustentar el ciclo de vida completo de las aplicaciones web: compilación, pruebas, implementación, administración y actualización, dentro del mismo entorno integrado.
+*/
